5f2c9f79eb2da6942782658073c50e6f627cc647
Christopher Reed <mail@seeread.info>
2016-10-29 12:16:34 -0400

    attempt for production friendly iframe src

--- app/helpers/easymarklet/link_helper.rb
+++ app/helpers/easymarklet/link_helper.rb
@@ -1,7 +1,7 @@
 module Easymarklet::LinkHelper
 
   def easymarklet_js(manifest = 'bookmarklet.js')
-    "javascript:(function(){var script=document.createElement('SCRIPT');script.src='#{root_url}assets/#{manifest}';document.body.appendChild(script);})()"
+    "javascript:(function(){var script=document.createElement('SCRIPT');script.src='#{root_url}#{manifest}';document.body.appendChild(script);})()"
   end
 
 end
